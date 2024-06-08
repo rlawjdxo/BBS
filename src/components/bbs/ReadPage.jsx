3d9731305ff5a18a746c6c75d5867d275d6d0be5
rlawjdxo <rlawjd0721@naver.com>
2024-06-09 00:02:23 +0900

    W14

--- src/components/bbs/ReadPage.jsx
+++ src/components/bbs/ReadPage.jsx
@@ -1,48 +1,61 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
-import {app} from '../firebaseInit'
-import { getFirestore, doc, getDoc } from 'firebase/firestore'
-import {Row, Col, Button, Card} from 'react-bootstrap'
+import { app } from '../firebaseInit'
+import { getFirestore, doc, getDoc, deleteDoc } from 'firebase/firestore'
+import { Row, Col, Button, Card } from 'react-bootstrap'
+import { useNavigate } from 'react-router-dom'
+import Comments from './Comments'
+
 const ReadPage = () => {
-    const loginEmail = sessionStorage.getItem('email')
-    const [post, setPost] = useState('')
+    const navi = useNavigate();
+    const loginEmail = sessionStorage.getItem('email');
+    const [post, setPost] = useState('');
     const {id} = useParams();
     const db = getFirestore(app);
-    const calAPI = async() => {
-    const res = await getDoc(doc(db, `posts/${id}`));
-    setPost(res.data());
-   }
+    const callAPI = async () => {
+        const res = await getDoc(doc(db, `posts/${id}`));
+        // console.log(res.data());
+        setPost(res.data());
+    }
+    const {email, date, title, contents} = post;
 
-   const {email, date, title, contents} = post;
+    useEffect(() => {
+        callAPI();
+    }, []);
 
-   useEffect(()=>{
-    calAPI();
-   },[]);
+    const onClickDelete = async () => {
+        if(!window.confirm(`${id}번 게시글을 삭제합니다.`)) return;
+        await deleteDoc(doc(db, `/posts/${id}`));
+        navi('/bbs');
+    }
 
-  return (
-    <Row className='my-5 justify-content-center'>
-        <Col xs={12} md={10} lg={8}>
-            <h1 className='mb-5'>게시글정보</h1>
-            {loginEmail===email &&
-                <div className='text-end mb-2'>
-                    <Button variant='success' size='sm' className='me-2 px-3'>수정</Button>
-                    <Button variant='danger' size='sm' className='px-3'>삭제</Button>
-                </div>
-            }
-            <Card>
-                <Card.Body>
-                    <h5>{title}</h5>
-                    <div className='text-muted'>
-                        <span className='me-2'>{date}</span>
-                        <span>{email}</span>
+    return (
+        <Row className='my-5 justify-content-center'>
+            <Col xs={12} md={10} lg={8}>
+                <h1>게시글 정보</h1>
+                {loginEmail === email &&
+                    <div className='text-end mb-2'>
+                        <Button onClick={()=>navi(`/bbs/update/${id}`)} variant='success' size='sm' className='me-2'>수정</Button>
+                        <Button onClick={onClickDelete} variant='danger' size='sm'>삭제</Button>
                     </div>
-                    <hr/>
-                    <div style={{whiteSpace:'pre-wrap'}}>{contents}</div>
-                </Card.Body>
-            </Card>
-        </Col>
-    </Row>
-  )
+                }
+                <Card>
+                    <Card.Body>
+                        <h5>{title}</h5>
+                        <div className='text-muted'>
+                            <span className='me-3'>{date}</span>
+                            <span>{email}</span>
+                        </div>
+                        <hr/>
+                        <div style={{whiteSpace:'pre-wrap'}}>
+                            {contents}
+                        </div>
+                    </Card.Body>
+                </Card>
+                <Comments/>
+            </Col>
+        </Row>
+    )
 }
 
 export default ReadPage
